b48cb7a0f87dbb55b9b78d4d3269eb0ea5f2f51e
joni <mesfinjohn@rocketmail.com>
2021-08-25 09:19:55 +0300

    before merge with deploy

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 const app = require('./app');
 const connectDatabase = require('./config/database');
 
-const dotenv = require('dotenv');
+// const dotenv = require('dotenv');
 
 // Handle Uncaught exceptions
 process.on('uncaughtException', err => {

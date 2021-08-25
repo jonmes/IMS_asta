5db96ed6d64f064898eeeae2da527c34d1c4af25
joni <mesfinjohn@rocketmail.com>
2021-08-25 14:22:52 +0300

    Request in progress

---
+++ backend/models/order.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const orderSchema = mongoose.Schema({
+    requestInfo: {
+        department: {
+            type: String,
+            required: true
+        },
+        phoneNo: {
+            type: String,
+            required: true
+        }
+    },
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User'
+    },
+    orderItems: [{
+        name: {
+            type: String,
+            required: true
+        },
+        quantity: {
+            type: Number,
+            required: true
+        },
+        product: {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Product'
+        }
+    }],
+    requestedAt: {
+        type: Date
+    },
+    orderStatus: {
+        type: String,
+        required: true,
+        default: 'Processing'
+    },
+    deliveredAt: {
+        type: Date
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+
+})
+
+module.exports = mongoose.model('Order', orderSchema)
